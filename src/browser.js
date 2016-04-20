94fddd8466d55d2e79f898859d4980e01d3609ea
apla <dot.and.thing@gmail.com>
2016-04-20 13:41:21 +0300

    load something by default

--- src/browser.js
+++ src/browser.js
@@ -54,6 +54,8 @@ document.addEventListener ('DOMContentLoaded', function () {
 		loadGithubRepositories (input.value);
 		e.preventDefault();
 	});
+
+	ViewEE.init (githubCORSUrl ('https://github.com/sparkfun/MPU-6050_Breakout/blob/master/Hardware/Triple_Axis_Accelerometer_-_Gyro_Breakout_-_MPU-6050.brd'));
 });
 
 function loadGithubRepositories (searchString) {
