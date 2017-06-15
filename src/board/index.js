6b2acbccdf643418ae5416087d5dc632e02be756
apla <dot.and.thing@gmail.com>
2017-06-15 08:09:39 +0300

    [es6] board refactor

---
+++ src/board/index.js
@@ -0,0 +1,186 @@
+import * as Util from '../util';
+
+import EagleXMLParser from './parser-eagle-xml';
+import KicadPcbParser from './parser-kicad-pcb';
+import GedaParser     from './parser-geda';
+
+/**
+ * Board's data container. It can contain Engineering data (such as pcb or schematics),
+ * Manufacturing data (gerber layers or 3d model data).
+ */
+export default class ViewEEBoard {
+
+	constructor () {
+		this.angleForRot  = Util.angleForRot;
+		this.matrixForRot = Util.matrixForRot;
+		this.calcBBox     = Util.calcBBox;
+
+		// this is a layers as seen in editor
+		this.layersCAD = [];
+
+		// this is a manufacturing layers, like gerber
+		this.layersCAM = [];
+	}
+
+	/**
+	 * Find a parser which supports data
+	 * @param   {String|Buffer} data string or binary data
+	 * @returns {Array}         compatible parsers
+	 */
+	static findParsers (data) {
+		const parsers = [
+			EagleXMLParser,
+			KicadPcbParser,
+			GedaParser
+		];
+
+		return parsers.filter (parser => {
+			if (!parser) return;
+
+			if (!parser.supports (data))
+				return;
+
+			return true;
+		});
+	}
+
+	static fromData (data, options) {
+		const parsers = ViewEEBoard.findParsers (data);
+
+		const board = new ViewEEBoard (options);
+
+		const timerLabel = `parsing using ${parser.name}`;
+		board.verbose && console.time (timerLabel);
+
+		// use first one, ignore others
+		const parser = new (parsers[0]) (board);
+
+		parser.parse (data);
+
+		board.verbose && console.timeEnd (timerLabel);
+
+		// WHY?
+		board.nativeBounds = board.calculateBounds();
+		board.nativeSize   = [
+			board.nativeBounds[2] - board.nativeBounds[0],
+			board.nativeBounds[3] - board.nativeBounds[1]
+		];
+		board.scaleToFit();
+		board.draw();
+
+		return board;
+	}
+
+	static fromFilename (filename) {
+
+	}
+
+	calculateBounds () {
+		var minX = LARGE_NUMBER,
+			minY = LARGE_NUMBER,
+			maxX = -LARGE_NUMBER,
+			maxY = -LARGE_NUMBER;
+
+		// Plain elements
+		Object.keys (this.plainWires).forEach (layerKey => {
+			const lines = this.plainWires[layerKey];
+			Object.keys (lines).forEach (lineKey => {
+				const line = lines[lineKey],
+					x1 = line.x1,
+					x2 = line.x2,
+					y1 = line.y1,
+					y2 = line.y2,
+					width = line.width || this.minLineWidth;
+				minX = Math.min (...[minX, x1-width, x1+width, x2-width, x2+width]);
+				maxX = Math.max (...[maxX, x1-width, x1+width, x2-width, x2+width]);
+				minY = Math.min (...[minY, y1-width, y1+width, y2-width, y2+width]);
+				maxY = Math.max (...[maxY, y1-width, y1+width, y2-width, y2+width]);
+			})
+		})
+
+		for (var netName in this.signalItems) {
+			for (var layerKey in this.signalItems[netName]) {
+				var lines = this.signalItems[netName][layerKey].wires;
+				for (var lineKey in lines) {
+					var line = lines[lineKey],
+						x1 = line.x1,
+						x2 = line.x2,
+						y1 = line.y1,
+						y2 = line.y2,
+						width = line.width || this.minLineWidth;
+					minX = Math.min (...[minX, x1-width, x1+width, x2-width, x2+width]);
+					maxX = Math.max (...[maxX, x1-width, x1+width, x2-width, x2+width]);
+					minY = Math.min (...[minY, y1-width, y1+width, y2-width, y2+width]);
+					maxY = Math.max (...[maxY, y1-width, y1+width, y2-width, y2+width]);
+				}
+			}
+		}
+
+		//Elements
+		for (var elemKey in this.elements) {
+			var elem = this.elements[elemKey];
+			var pkg = typeof elem.pkg === "string" ? this.packagesByName[elem.pkg] : elem.pkg;
+			var rotMat = elem.matrix;
+			for (var smdIdx in pkg.smds) {
+				var smd = pkg.smds[smdIdx],
+					x1 = elem.x + rotMat[0]*smd.x1 + rotMat[1]*smd.y1,
+					y1 = elem.y + rotMat[2]*smd.x1 + rotMat[3]*smd.y1,
+					x2 = elem.x + rotMat[0]*smd.x2 + rotMat[1]*smd.y2,
+					y2 = elem.y + rotMat[2]*smd.x2 + rotMat[3]*smd.y2;
+				minX = Math.min (...[minX, x1, x2]);
+				maxX = Math.max (...[maxX, x1, x2]);
+				minY = Math.min (...[minY, y1, y2]);
+				maxY = Math.max (...[maxY, y1, y2]);
+			}
+			for (var wireIdx in pkg.wires) {
+				var wire = pkg.wires[wireIdx],
+					x1 = elem.x + rotMat[0]*wire.x1 + rotMat[1]*wire.y1,
+					y1 = elem.y + rotMat[2]*wire.x1 + rotMat[3]*wire.y1,
+					x2 = elem.x + rotMat[0]*wire.x2 + rotMat[1]*wire.y2,
+					y2 = elem.y + rotMat[2]*wire.x2 + rotMat[3]*wire.y2,
+					width = wire.width || this.minLineWidth;
+				minX = Math.min (...[minX, x1-width, x1+width, x2-width, x2+width]);
+				maxX = Math.max (...[maxX, x1-width, x1+width, x2-width, x2+width]);
+				minY = Math.min (...[minY, y1-width, y1+width, y2-width, y2+width]);
+				maxY = Math.max (...[maxY, y1-width, y1+width, y2-width, y2+width]);
+			}
+		}
+
+		// console.log ("board size:", [minX, minY, maxX, maxY]);
+
+		return [minX, minY, maxX, maxY];
+	}
+}
+
+/**
+ * Function to fill out the data object. Parser should decide itself
+ * what data to fill or clean up.
+ * @param   {string}  text data to fill the board object
+ */
+ViewEEBoard.prototype.parseFile = function (text) {
+	var parserFound = ViewEE.parsers.some (function (parser) {
+		if (!parser) return;
+
+		if (parser.supports (text)) {
+			var timerLabel = 'parsing using ' + parser.name;
+			this.verbose && console.time && console.time (timerLabel);
+			var parser = new parser (this);
+			parser.parse (text);
+			this.verbose && console.timeEnd && console.timeEnd (timerLabel);
+
+			this.emit ('parse-end');
+
+			this.nativeBounds = this.calculateBounds();
+			this.nativeSize   = [
+				this.nativeBounds[2] - this.nativeBounds[0],
+				this.nativeBounds[3] - this.nativeBounds[1]
+			];
+			this.scaleToFit();
+			this.draw();
+			return true;
+		}
+	}, this);
+
+	if (!parserFound)
+		alert ('cannot find parser for selected file');
+}
